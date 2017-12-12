5b3cbc986cca80591647742cafb8ac0eb6fecf57
meenakshi.c <meenakshi.c@DS-D0278889C18F.HCLT.CORP.HCL.IN>
2017-12-12 15:47:49 +0530

    First Commit

--- TestProject/src/testSample.java
+++ TestProject/src/testSample.java
@@ -3,7 +3,10 @@ public class testSample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
+		//sample program
+		
+		System.out.println("this is the simulation of git with eclipse");
 	}
 
 }
